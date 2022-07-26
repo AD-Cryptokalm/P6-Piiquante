0f11e9116d7ef79ca9199c35d7aefe8a8bcb668c
AD-code <cryptokalm82@gmail.com>
2022-07-26 03:24:12 +0200

    version finale sécurisée

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,26 @@
-// importer express
+// importer express pour création des routes
 const express = require("express");
 
 const bodyParser = require("body-parser");
-// importer mongoose
+// importer mongoose pour les schemas
 const mongoose = require("mongoose");
+
+// importer helmet
+const helmet = require("helmet");
+
+// importer dotenv pour les variables d'environnement
+require('dotenv').config(); 
 
 // importer les routers
 const saucesRoutes = require("./routes/sauces");
 const userRoutes = require("./routes/user");
 
-const path = require('path');
+const path = require("path");
 
 // connection au server mongoose
 mongoose
   .connect(
-    "mongodb+srv://AD115:aUqqT2sMJ6QGsx4@cluster0.y06xs7s.mongodb.net/?retryWrites=true&w=majority",
+    `mongodb+srv://${process.env.MDB_USERNAME}:${process.env.MDB_PASSWORD}@${process.env.MDB_CLUSTER}.mongodb.net/?retryWrites=true&w=majority`,
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => console.log("Connexion à MongoDB réussie !"))
@@ -44,7 +50,11 @@ app.use("/api/sauces", saucesRoutes);
 // pour cette route utiliser la route userRoutes
 app.use("/api/auth", userRoutes);
 
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+app.use(helmet());
+
+
 
 // exporter const app pour y acceder depuis les autres fichiers
 module.exports = app;
